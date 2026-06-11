4ecec7514f4a8f8026a3bc0f4abfdcc32d589352
Angelina Amy Sulca Cuba <at61291576@idat.pe>
2026-06-11 09:04:45 -0500

    entrega 2 ( aun falta )

--- pages/angelina_sulca/setencias.js
+++ pages/angelina_sulca/setencias.js
@@ -13,8 +13,32 @@ if (monto > 100) {
 let monto = 120;
 
 if (monto >= 100 ) {
-    console.log("tiene un descuento del 10%");
+    console.log("Tiene un descuento del 3%");
 } else {
-    console.log("no tiene descuento");
+    console.log("No tiene descuento");
 }
 
+/// if - else if - else
+    
+let monto = 120;
+
+if (monto >= 300) {
+    console.log("Tiene un descuento de el 5%");
+} else if (nota >= 100) {
+    console.log("Tiene un descuento del 3%");
+} else {
+    console.log("No tiene descuentos");
+}
+
+/// sintaxis
+
+let monto = 120;
+
+let mensaje =
+    monto >= 300
+        ? "Promoción de descuento aplicada"
+        : "Sin promoción de descuento";
+
+console.log(mensaje);
+
+/// switch
