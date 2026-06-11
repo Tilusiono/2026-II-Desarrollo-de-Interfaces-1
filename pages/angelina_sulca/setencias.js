a7be33b8bc3db90e8b5d508741c94e68086e0ff1
Angelina Amy Sulca Cuba <at61291576@idat.pe>
2026-06-11 08:48:13 -0500

    entrega 1 ( aun falta )

---
+++ pages/angelina_sulca/setencias.js
@@ -0,0 +1,20 @@
+///sistema de ventas de una bodega
+
+///if
+
+let monto = 120;
+
+if (monto > 100) {
+    console.log("Es mayor de 100 soles");
+}
+
+/// if - else
+
+let monto = 120;
+
+if (monto >= 100 ) {
+    console.log("tiene un descuento del 10%");
+} else {
+    console.log("no tiene descuento");
+}
+
