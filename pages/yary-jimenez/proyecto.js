319af44ade8c73dc0370897b44c98422e91e9082
Yary Jimenez Celis <at72955519@idat.pe>
2026-06-16 10:08:10 -0600

    Cambio yary-jimenez 06-16-2026_10-08

---
+++ pages/yary-jimenez/proyecto.js
@@ -0,0 +1,129 @@
+class Cafeteria {
+    nombre;
+    tipoCafe;
+    capacidad;
+}
+
+class Vendedor {
+    nombre;
+    apellido;
+    turno;
+}
+
+class Cliente {
+    nombre;
+    telefono;
+    correo;
+}
+
+class Producto {
+    nombreProducto;
+    categoria;
+    stock;
+}
+
+class Precio {
+    precioUnitario;
+    descuento;
+    precioFinal;
+}
+
+class Ubicacion {
+    direccion;
+    distrito;
+    referencia;
+}
+
+class Horario {
+    horaApertura;
+    horaCierre;
+    diasAtencion;
+}
+
+class Telefono {
+    numeroPrincipal;
+    numeroSecundario;
+    codigoPais;
+}
+
+class Correo {
+    correoPrincipal;
+    correoSoporte;
+    dominio;
+}
+
+class Venta {
+    fecha;
+    cantidad;
+    total;
+}
+
+class Cafeteria {
+    nombre;
+    tipoCafe;
+    capacidad;
+}class Cafeteria {
+    nombre;
+    tipoCafe;
+    capacidad;
+}
+
+class Vendedor {
+    nombre;
+    apellido;
+    turno;
+}
+
+class Cliente {
+    nombre;
+    telefono;
+    correo;
+}
+
+class Producto {
+    nombreProducto;
+    categoria;
+    stock;
+}
+
+class Precio {
+    precioUnitario;
+    descuento;
+    precioFinal;
+}
+
+class Ubicacion {
+    direccion;
+    distrito;
+    referencia;
+}
+
+class Horario {
+    horaApertura;
+    horaCierre;
+    diasAtencion;
+}
+
+class Telefono {
+    numeroPrincipal;
+    numeroSecundario;
+    codigoPais;
+}
+
+class Correo {
+    correoPrincipal;
+    correoSoporte;
+    dominio;
+}
+
+class Venta {
+    fecha;
+    cantidad;
+    total;
+}
+
+class Cafeteria {
+    nombre;
+    tipoCafe;
+    capacidad;
+}
