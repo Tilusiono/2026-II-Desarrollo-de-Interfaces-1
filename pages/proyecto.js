6640ef2408f70659c164e70c0f54b2fe5076a21e
Osvaldo Farid Sanchez Gallupe <at73167929@idat.pe>
2026-06-16 11:03:54 -0500

    Cambio osvaldo-sanchez 06-16-2026_11-03

---
+++ pages/proyecto.js
@@ -0,0 +1,39 @@
+class alumno{
+    nombre; 
+    apellido; 
+    edad; number;
+    telmovil; 
+
+
+    constructor(nom, ape, edad, tel){
+        this.nombre = nom
+        this.apellido = ape
+        this.edad=edad;
+        this.telmovil=tel
+    }
+     constructor(nom, ape,edad, tel){
+        
+     }
+
+}
+
+class  profesor{
+    nombre; String;
+    apellido; String;
+    FechaNac; String;
+    nrotelefono;
+    gradoAcademico;
+}
+
+class  notas {
+    FechaRegistro: Date;
+    FechaCierre:Date;
+    Nota; number;
+    NotaPrevia; number
+} 
+
+class  cursos{
+    nombre;
+    descripcion;
+    ;
+} 
