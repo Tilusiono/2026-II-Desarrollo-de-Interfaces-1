f6ea6f78a5084088f29003dcf383d82b9f07f900
Jose Luis Mendoza Ventura <iv72817878@idat.pe>
2026-06-16 10:50:56 -0500

    Cambio jose-mendoza 06-16-2026_10-50

---
+++ pages/jose-mendoza/Proyecto.js
@@ -0,0 +1,28 @@
+// marketing proyecto ejercicio
+
+class empleado{
+    nombre;
+    apellido;
+    edad;
+    movil;
+    sectorTrabajo;
+}
+
+class jefe{
+    nombreCompletos;
+    edad;
+    nroTelefono;
+    areaResponsable;
+}
+
+class horas_trabajadas{
+    horas_normales;
+    horas_extras;
+    fecha;
+}
+
+class dias{
+    fecha;
+    nombreDia;
+    asistencia;
+}
