2a9c99c7a1a2274bede2ddd7dc74b1e2f54caae9
Jose Carlos Villanueva Caycho <at70720325@idat.pe>
2026-06-18 08:54:25 -0500

    Cambio jose-villanueva 06-18-2026_08-54

--- pages/jose-villanueva/sentencias.js/proyecto.js
+++ pages/jose-villanueva/sentencias.js/proyecto.js
@@ -1,52 +1,131 @@
 class Alumno {
-    idAlumno;         
-    nombre;
-    apellido;
-    edad;
-    correoElectronico;
-    telefono;
-    gradoAcademico;  
-    estado; 
-
-    constructor(id, nombre, apellido) {
+    constructor(id, nombre, apellido, edad, correo, telefono, grado, estado) {
         this.idAlumno = id;
         this.nombre = nombre;
         this.apellido = apellido;
+        this.edad = edad;
+        this.correoElectronico = correo;
+        this.telefono = telefono;
+        this.gradoAcademico = grado;
+        this.estado = estado;
     }
 }
 
-class Alumno {
-    idAlumno; nombre; apellido; edad; correoElectronico; telefono; gradoAcademico; estado; 
-}
-
 class Profesor {
-    idProfesor; nombre; apellido; edad; correoElectronico; telefono; especialidad; fechaContratacion;
+    constructor(id, nombre, apellido, edad, correo, telefono, especialidad, fecha) {
+        this.idProfesor = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correoElectronico = correo;
+        this.telefono = telefono;
+        this.especialidad = especialidad;
+        this.fechaContratacion = fecha;
+    }
 }
 
 class Notas {
-    idNota; idAlumno; idCurso; calificacion; tipoEvaluacion; fecha; 
+    constructor(id, idAlumno, idCurso, calificacion, tipo, fecha) {
+        this.idNota = id;
+        this.idAlumno = idAlumno;
+        this.idCurso = idCurso;
+        this.calificacion = calificacion;
+        this.tipoEvaluacion = tipo;
+        this.fecha = fecha;
+    }
 }
 
 class Curso {
-    idCurso; nombreCurso; codigoCurso; idProfesor; creditos; horario; 
+    constructor(id, nombre, codigo, idProfesor, creditos, horario) {
+        this.idCurso = id;
+        this.nombreCurso = nombre;
+        this.codigoCurso = codigo;
+        this.idProfesor = idProfesor;
+        this.creditos = creditos;
+        this.horario = horario;
+    }
 }
 
-class Matricula {  
-    idMatricula; idAlumno; idCurso; periodoAcademico; fechaInscripcion; 
+class Matricula {
+    constructor(id, idAlumno, idCurso, periodo, fecha) {
+        this.idMatricula = id;
+        this.idAlumno = idAlumno;
+        this.idCurso = idCurso;
+        this.periodoAcademico = periodo;
+        this.fechaInscripcion = fecha;
+    }
 }
 
 class Asistencia {
-    idAsistencia; idAlumno; idCurso; fecha; estadoAsistencia; observaciones;
+    constructor(id, idAlumno, idCurso, fecha, estado, observaciones) {
+        this.idAsistencia = id;
+        this.idAlumno = idAlumno;
+        this.idCurso = idCurso;
+        this.fecha = fecha;
+        this.estadoAsistencia = estado;
+        this.observaciones = observaciones;
+    }
 }
 
 class Aula {
-    idAula; nombreAula; capacidad; pabellon; tipoAula; 
+    constructor(id, nombre, capacidad, pabellon, tipo) {
+        this.idAula = id;
+        this.nombreAula = nombre;
+        this.capacidad = capacidad;
+        this.pabellon = pabellon;
+        this.tipoAula = tipo;
+    }
 }
 
 class Carrera {
-    idCarrera; nombreCarrera; codigoCarrera; duracionSemestres; directorCarrera; estado;
+    constructor(id, nombre, codigo, duracion, director, estado) {
+        this.idCarrera = id;
+        this.nombreCarrera = nombre;
+        this.codigoCarrera = codigo;
+        this.duracionSemestres = duracion;
+        this.directorCarrera = director;
+        this.estado = estado;
+    }
 }
 
 class Pago {
-    idPago; idAlumno; monto; fechaPago; concepto; estadoPago; metodoPago;
+    constructor(id, idAlumno, monto, fecha, concepto, estado, metodo) {
+        this.idPago = id;
+        this.idAlumno = idAlumno;
+        this.monto = monto;
+        this.fechaPago = fecha;
+        this.concepto = concepto;
+        this.estadoPago = estado;
+        this.metodoPago = metodo;
+    }
 }
+
+class Departamento {
+    constructor(id, nombre, jefe, ubicacion, estado) {
+        this.idDepartamento = id;
+        this.nombreDepartamento = nombre;
+        this.jefeDepartamento = jefe;
+        this.ubicacion = ubicacion;
+        this.estado = estado;
+    }
+}
+
+class Producto {
+    constructor(id, nombre, categoria, precio, stock, marca) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+        this.marca = marca;
+    }
+}
+
+let producto1 = new Producto(
+    101,
+    "Mouse Gamer",
+    "Periféricos",
+    120,
+    25,
+    "Logitech"
+);
