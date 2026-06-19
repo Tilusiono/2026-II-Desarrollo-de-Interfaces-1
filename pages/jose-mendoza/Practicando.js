09cc939d5cc7e17d4ca8af4476df0c165c24ee14
Bernabe Daniel Inche Ticlavilca <85372343+DAMN1SELF@users.noreply.github.com>
2026-06-19 05:22:38 -0500

    Sincronizar main conservando solo pages/yary-jimenez

--- pages/jose-mendoza/Practicando.js
+++
@@ -1,9 +0,0 @@
-class Producto {
-    constructor(persona, perro, stock) {
-        this.persona = persona;
-        this.perro = perro;
-        this.stock = stock;
-    }
-}
-
-let Producto1 = new Producto("Daniel", "Jashi", 5);
