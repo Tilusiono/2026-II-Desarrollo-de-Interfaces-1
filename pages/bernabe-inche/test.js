9b70e4ed5cf9e0497e243e79f35f30f457df579f
Bernabe Daniel Inche Ticlavilca <85372343+DAMN1SELF@users.noreply.github.com>
2026-06-19 07:42:58 -0500

    Actualizar versión final de bernabe-inche

--- pages/bernabe-inche/test.js
+++ pages/bernabe-inche/test.js
@@ -8,7 +8,10 @@ const nombre="Bernabé";
 console.log(nombre);
 console.log(typeof nombre);
 
+const apellido_paterno="Inche";
+console.log(apellido_paterno);
+console.log(typeof apellido_paterno);
 
-const apellido="Inche";
-console.log(apellido);
-console.log(typeof apellido);
+const apellido_materno="Ticlavilca";
+console.log(apellido_materno);
+console.log(typeof apellido_materno);
