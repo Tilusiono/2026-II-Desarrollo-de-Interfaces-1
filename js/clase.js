d513b6733830cba4c530f205295793784231a6e3
Bernabe Daniel Inche Ticlavilca <85372343+DAMN1SELF@users.noreply.github.com>
2026-06-19 02:31:04 -0500

    arreglo de objetos, cada objeto representa un alumno con sus propiedades y métodos.

--- js/clase.js
+++ js/clase.js
@@ -23,19 +23,19 @@ class Alumno {
      * @param {string} [telefonoAlumno=""] Teléfono del alumno.
      * @param {Date} [fechaNacimiento=new Date()] Fecha de nacimiento del alumno.
      */
-    constructor(id,nombreAlumno, apellidoAlumno ="Sin Apellido", edadAlumno =0, telefonoAlumno = "", fechaNacimiento = new Date()) {
+    constructor(id, nombreAlumno, apellidoAlumno = "Sin Apellido", edadAlumno = 0, telefonoAlumno = "", fechaNacimiento = new Date()) {
 
-        if(typeof id !== "number")
+        if (typeof id !== "number")
             throw new Error("ID debe ser number");
-        if(typeof nombreAlumno !== "string")
+        if (typeof nombreAlumno !== "string")
             throw new Error("Nombre debe ser string");
-        if(typeof apellidoAlumno !== "string")
+        if (typeof apellidoAlumno !== "string")
             throw new Error("Apellido debe ser string");
-        if(typeof edadAlumno !== "number")
+        if (typeof edadAlumno !== "number")
             throw new Error("Edad debe ser number");
-        if(typeof telefonoAlumno !== "string")
+        if (typeof telefonoAlumno !== "string")
             throw new Error("Teléfono debe ser string");
-        if(!(fechaNacimiento instanceof Date))
+        if (!(fechaNacimiento instanceof Date))
             throw new Error("Fecha de nacimiento debe ser una instancia de Date");
         // Asignación de propiedades privadas
         this.#id = id;
@@ -51,11 +51,11 @@ class Alumno {
     }
 
     mostrarEstado() {
-        
-        if (this.#validarEdad() ) {
-            return "Es mayor de edad - "+ this.#edad;
+
+        if (this.#validarEdad()) {
+            return "Es mayor de edad - " + this.#edad;
         } else {
-            return "Es menor de edad - "+ this.#edad;
+            return "Es menor de edad - " + this.#edad;
         }
     }
 
@@ -81,21 +81,23 @@ class Alumno {
 
 
 
-let a0 = new Alumno("Bernabe");
-let a1 = new Alumno("Jose", "Rojas");
-let a2 = new Alumno("Felix", "Rodriguez", 22);
-let a3 = new Alumno("Ana", "Mejia", 22, "999421499"); 
-let a4 = new Alumno("Daniel","Inche",32,"919759571", new Date("1993-12-15"));
+let a0 = new Alumno(1, "Bernabe");
+let a1 = new Alumno(2, "Jose", "Rojas");
+let a2 = new Alumno(3, "Felix", "Rodriguez", 22);
+let a3 = new Alumno(4, "Ana", "Mejia", 22, "999421499");
+let a4 = new Alumno(5, "Daniel", "Inche", 32, "919759571", new Date("1993-12-15"));
+
+
 
 console.log(a3.getNombre());
 a3.setNombre("Ana Luisa");
 console.log(a3.getNombre());
 
-console.log(a4.getNombre()); 
+console.log(a4.getNombre());
 
 
-console.dir(a4); 
-console.log(a4); 
+console.dir(a4);
+console.log(a4);
 console.log(a4.fechaNacimiento);
 
 console.log(a0.mostrarEstado());
@@ -103,6 +105,17 @@ console.log(a1.mostrarEstado());
 console.log(a2.mostrarEstado());
 console.log(a3.mostrarEstado());
 console.log(a4.mostrarEstado());
+
+
+
+let alumnos = [
+    a0, a1, a2, a3, a4,
+    new Alumno(6, "Jean", "Perez", 17, "987654321", new Date("2006-05-20"))
+
+];
+
+console.log(alumnos)
+
 /*
 Tipos de datos en JavaScript
 
