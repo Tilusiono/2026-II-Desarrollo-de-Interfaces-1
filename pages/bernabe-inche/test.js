d260afb3dfe1e36465a57762f645b4d6588093da
Bernabe Daniel Inche Ticlavilca <85372343+DAMN1SELF@users.noreply.github.com>
2026-06-19 07:11:54 -0500

    Sincronizar main conservando pages/bernabe-inche y carpeta git

--- pages/bernabe-inche/test.js
+++ pages/bernabe-inche/test.js
@@ -7,7 +7,3 @@ console.log(typeof numero);
 const nombre="Bernabé";
 console.log(nombre);
 console.log(typeof nombre);
-
-const apellido="Inche";
-console.log(apellido);
-console.log(typeof apellido);
