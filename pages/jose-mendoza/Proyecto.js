af73043c674147fcf853d0622b8955b49fc76fc9
Jose Luis Mendoza Ventura <iv72817878@idat.pe>
2026-06-19 15:41:54 -0500

    Cambio jose-mendoza 06-19-2026_15-41

--- pages/jose-mendoza/Proyecto.js
+++ pages/jose-mendoza/Proyecto.js
@@ -1,60 +1,179 @@
-// marketing proyecto ejercicio
-
 class Empleado{
-    nombre;
-    apellido;
-    edad;
-    movil;
-    sectorTrabajo;
+    #idEmpleado;
+    #nombre;
+    #apellido;
+    #edad;
+    #movil;
+    #sector;
 
-    constructor(nom, ape, edad, movil, sector){
-        this.nombre = nom;
-        this.apellido = ape;
-        this.edad = edad;
-        this.movil = movil;
-        this.sectorTrabajo = sector;
+    constructor(id, nombre, apellido, edad, movil, sector){
+        this.#idEmpleado = id;
+        this.#nombre = nombre;
+        this.#apellido = apellido;
+        this.#edad = edad;
+        this.#movil = movil;
+        this.#sector = sector;
+    }
+
+    mostrarEmpleado(){
+        console.log(this.#idEmpleado, this.#nombre, this.#apellido);
+    }
+}
+
+class Sector{
+    #idSector;
+    #nombreSector;
+    #descripcion;
+
+    constructor(id, nombre, descripcion){
+        this.#idSector = id;
+        this.#nombreSector = nombre;
+        this.#descripcion = descripcion;
+    }
+
+    mostrarSector(){
+        console.log(this.#nombreSector);
     }
 }
 
 class Jefe{
-    nombreCompletos;
-    edad;
-    nroTelefono;
-    areaResponsable;
+    #idJefe;
+    #nombre;
+    #edad;
+    #telefono;
+    #area;
 
-    constructor(nombre, edad, telefono, area){
-        this.nombreCompletos = nombre;
-        this.edad = edad;
-        this.nroTelefono = telefono;
-        this.areaResponsable = area;
+    constructor(id, nombre, edad, telefono, area){
+        this.#idJefe = id;
+        this.#nombre = nombre;
+        this.#edad = edad;
+        this.#telefono = telefono;
+        this.#area = area;
+    }
+
+    mostrarJefe(){
+        console.log(this.#nombre, this.#area);
+    }
+}
+
+class Asistencia{
+    #idAsistencia;
+    #fecha;
+    #estado;
+    #idEmpleado;
+
+    constructor(id, fecha, estado, empleado){
+        this.#idAsistencia = id;
+        this.#fecha = fecha;
+        this.#estado = estado;
+        this.#idEmpleado = empleado;
+    }
+
+    mostrarAsistencia(){
+        console.log(this.#fecha, this.#estado);
     }
 }
 
 class HorasTrabajadas{
-    horas_normales;
-    horas_extras;
-    fecha;
-    total_horas;
+    #idHoras;
+    #horasNormales;
+    #horasExtras;
+    #totalHoras;
+    #fecha;
+    #idEmpleado;
 
-    constructor(normales, extras, fecha){
-        this.horas_normales = normales;
-        this.horas_extras = extras;
-        this.fecha = fecha;
-        this.total_horas = normales + extras;
+    constructor(id, normales, extras, fecha, empleado){
+        this.#idHoras = id;
+        this.#horasNormales = normales;
+        this.#horasExtras = extras;
+        this.#totalHoras = normales + extras;
+        this.#fecha = fecha;
+        this.#idEmpleado = empleado;
+    }
+
+    mostrarHoras(){
+        console.log(this.#totalHoras);
     }
 }
 
-class Dias{
-    fecha;
-    nombreDia;
-    asistencia;
-    feriado;
+class Permiso{
+    #idPermiso;
+    #motivo;
+    #fechaInicio;
+    #fechaFin;
+    #idEmpleado;
 
-    constructor(fecha, nombreDia, asistencia, feriado){
-        this.fecha = fecha;
-        this.nombreDia = nombreDia;
-        this.asistencia = asistencia;
-        this.feriado = feriado;
+    constructor(id, motivo, inicio, fin, empleado){
+        this.#idPermiso = id;
+        this.#motivo = motivo;
+        this.#fechaInicio = inicio;
+        this.#fechaFin = fin;
+        this.#idEmpleado = empleado;
     }
 }
 
+class Vacaciones{
+    #idVacacion;
+    #fechaInicio;
+    #fechaFin;
+    #idEmpleado;
+
+    constructor(id, inicio, fin, empleado){
+        this.#idVacacion = id;
+        this.#fechaInicio = inicio;
+        this.#fechaFin = fin;
+        this.#idEmpleado = empleado;
+    }
+}
+
+class Feriado{
+    #idFeriado;
+    #fecha;
+    #descripcion;
+
+    constructor(id, fecha, descripcion){
+        this.#idFeriado = id;
+        this.#fecha = fecha;
+        this.#descripcion = descripcion;
+    }
+
+    mostrarFeriado(){
+        console.log(this.#fecha, this.#descripcion);
+    }
+}
+
+class Pago{
+    #idPago;
+    #monto;
+    #fechaPago;
+    #idEmpleado;
+
+    constructor(id, monto, fecha, empleado){
+        this.#idPago = id;
+        this.#monto = monto;
+        this.#fechaPago = fecha;
+        this.#idEmpleado = empleado;
+    }
+
+    mostrarPago(){
+        console.log(this.#monto);
+    }
+}
+
+class Reporte{
+    #idReporte;
+    #fecha;
+    #tipo;
+    #idJefe;
+
+    constructor(id, fecha, tipo, jefe){
+        this.#idReporte = id;
+        this.#fecha = fecha;
+        this.#tipo = tipo;
+        this.#idJefe = jefe;
+    }
+
+    mostrarReporte(){
+        console.log(this.#tipo);
+    }
+}
