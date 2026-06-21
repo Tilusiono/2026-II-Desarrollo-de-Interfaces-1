3ac58d9e39ac2f98bc35d8f94a02757217d191f9
Maycol Jhonatan Esthiben Puchuri Mezaraime <at74096922@idat.pe>
2026-06-21 11:46:57 -0500

    Cambio maycol-puchuri 06-21-2026_11-46

--- js/proyecto.js
+++ js/proyecto.js
@@ -1,3 +1,97 @@
-console.log("Test");
-console.log("Demo");
-/***test */
+class Persona {
+  #edad;
+
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.#edad = edad;
+  }
+
+  getEdad() {
+    return this.#edad;
+  }
+}
+
+class Estudiante {
+  #promedio;
+
+  constructor(nombre, promedio) {
+    this.nombre = nombre;
+    this.#promedio = promedio;
+  }
+
+  getPromedio() {
+    return this.#promedio;
+  }
+}
+
+class Profesor {
+  #salario;
+
+  constructor(nombre, salario) {
+    this.nombre = nombre;
+    this.#salario = salario;
+  }
+}
+
+class Curso {
+  #codigo;
+
+  constructor(nombre, codigo) {
+    this.nombre = nombre;
+    this.#codigo = codigo;
+  }
+}
+
+class Libro {
+  #precio;
+
+  constructor(titulo, precio) {
+    this.titulo = titulo;
+    this.#precio = precio;
+  }
+}
+
+class Biblioteca {
+  #cantidadLibros;
+
+  constructor(nombre, cantidadLibros) {
+    this.nombre = nombre;
+    this.#cantidadLibros = cantidadLibros;
+  }
+}
+
+class Producto {
+  #stock;
+
+  constructor(nombre, stock) {
+    this.nombre = nombre;
+    this.#stock = stock;
+  }
+}
+
+class Cliente {
+  #dni;
+
+  constructor(nombre, dni) {
+    this.nombre = nombre;
+    this.#dni = dni;
+  }
+}
+
+class Vehiculo {
+  #placa;
+
+  constructor(marca, placa) {
+    this.marca = marca;
+    this.#placa = placa;
+  }
+}
+
+class Empresa {
+  #ruc;
+
+  constructor(nombre, ruc) {
+    this.nombre = nombre;
+    this.#ruc = ruc;
+  }
+}
