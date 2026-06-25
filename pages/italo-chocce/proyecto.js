f2dc4ae7a71961224ff5d2ef7696a0326c6d838b
Jheff <jalbornoz@idat.pe>
2026-06-25 07:21:19 -0500

    Actualizar versión final de italo-chocce

--- pages/italo-chocce/proyecto.js
+++ pages/italo-chocce/proyecto.js
@@ -1,49 +1,126 @@
 /**proyecto casino Las Perlitas De Barcena */
 
 class usuario {
-    id;
-    nombre;
-    edad;
-    corre_electronico;
-    telefono;
+    Constructor(id, nombre, edad, correo_electronico, telefono) {
+        
+        if (edad < 18) {
+            throw new Error("El usuario debe ser mayor de edad para registrarse en el casino.");
+        }
+    
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo_electronico = correo_electronico;
+        this.telefono = telefono;
+        console.log("Usuario registrado exitosamente.");
 }
 
+}
+
+class recepcionista {
+    Constructor(id_empleado, nombre_empleado, turno) {
+        this.id_empleado = id_empleado;
+        this.nombre_empleado = nombre_empleado;
+        this.turno = turno;
+    }
+    
+}
 class Billetera_virtual {
-    fondos_restantes;
+    constructor(id_usuario, fondos_restantes) {
+        this.id_usuario = id_usuario;   
+        this.fondos_restantes = fondos_restantes;
+   
     retirar_fondos;
     depositar_fondos;
 }
 
+}
 class historial {
-    Ingresos_recibidos;
-    Ingresos_Perdidos;
-    cantidades_apostadas;
+
+    constructor(id_usuario, transacciones) {
+        this.id_usuario = id_usuario    
+        this.transacciones = transacciones;
+        this.fecha_transaccion;
+        this.monto_transaccion;
+        this.tipo_transaccion;
+    }
 }
 
 class Fichas {
-    compra_fichas;
-    cambiar_fichas;
+    Constructor(id_usuario, cantidad_fichas) {
+        this.id_usuario = id_usuario;
+        this.cantidad_fichas = cantidad_fichas;
+        this.valor_ficha;
+        this.fecha_adquisicion;
+    }
+    
+    
 }
 
 class Mesa {
-    id_mesa;
-    apuesta_maxima;
-    apuesta_minima;
-    juego;
-    usuariosJugando;
-}
+    constructor(id_mesa, apuesta_maxima, apuesta_minima, juego) {
+        this.id_mesa = id_mesa;
+        this.apuesta_maxima = apuesta_maxima;
+        this.apuesta_minima = apuesta_minima;
+        this.juego = juego;
+        this.usuariosJugando = [];
 
+}
+ 
+}
 class apuesta {
-    id_usuario;
-    apuesta_realizada;
-    estado;
-    prediccion;
+    constructor(id_usuario, apuesta_realizada, estado, prediccion) {
+        this.id_usuario = id_usuario;
+        this.apuesta_realizada = apuesta_realizada;
+        this.estado = estado;
+        this.prediccion = prediccion;
+    }
 
 }
 
 class Juez_mesa{
-        id_empleado;
-        nombre_empleado;
-        mesa_asignada;
+    constructor(id_empleado, nombre_empleado, mesa_asignada) {
+         this.id_empleado = id_empleado;
+         this.nombre_empleado = nombre_empleado;
+         this.mesa_asignada = mesa_asignada;
 
 }
+
+}
+
+class partida {
+    constructor(id_partida, id_mesa, usuarios_participantes) {
+        this.id_partida = id_partida;
+        this.id_mesa = id_mesa;
+        this.usuarios_participantes = usuarios_participantes;
+    }
+
+}
+    
+
+
+class recompensa {
+    constructor(id_recompensa, descripcion) {
+        this.id_recompensa = id_recompensa;
+        this.descripcion = descripcion;
+    }
+}
+   
+
+console.log(usuario)
+console.log(recepcionista)
+console.log(Billetera_virtual)
+console.log(historial)
+console.log(Fichas)
+console.log(Mesa)
+console.log(apuesta)
+console.log(Juez_mesa)
+console.log(partida)
+console.log(recompensa)
+
+
+
+
+
+
+
