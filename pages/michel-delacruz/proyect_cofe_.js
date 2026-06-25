b995aa7f0d8216e81d54a73c9b47499bbc5c6268
Michel Roberto Carlos Delacruz Suarez <at73933331@idat.pe>
2026-06-24 20:42:44 -0500

    Cambio michel-delacruz 06-24-2026_20-42

--- pages/michel-delacruz/proyect_cofe_.js
+++ pages/michel-delacruz/proyect_cofe_.js
@@ -1,11 +1,44 @@
-class PRODUCTO {
+class ProductoCafe {
 
-    // Propiedades privadas
     #categoria;
-    #nombre;
-    #id;
-    #tamaño
-    #discripcion;
+    #nombreProducto;
+    #codigoCliente;
+    #nombreCliente;
+    #descripcion;
     #precio;
+    #cantidad;
 
+    constructor(
+        categoria,
+        nombreProducto,
+        codigoCliente,
+        nombreCliente,
+        descripcion,
+        precio,
+        cantidad
+    ) {
+        this.#categoria = categoria;
+        this.#nombreProducto = nombreProducto;
+        this.#codigoCliente = codigoCliente;
+        this.#nombreCliente = nombreCliente;
+        this.#descripcion = descripcion;
+        this.#precio = precio;
+        this.#cantidad = cantidad;
+    }
+
+    mostrarStock() {
+        if (this.#cantidad > 0) {
+            return "Producto disponible - Stock: " + this.#cantidad;
+        } else {
+            return "Producto agotado";
+        }
+    }
+
+    getNombreProducto() {
+        return this.#nombreProducto;
+    }
+
+    setNombreProducto(nombre) {
+        this.#nombreProducto = nombre;
+    }
 }
