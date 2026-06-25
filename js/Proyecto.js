340528feef2f2d983de949dd13f0cebe3aeb17a1
Bernabe Daniel Inche Ticlavilca <85372343+DAMN1SELF@users.noreply.github.com>
2026-06-25 05:29:30 -0500

    clase

---
+++ js/Proyecto.js
@@ -0,0 +1,80 @@
+import AlumnoNacional from "./AlumnoNacional.js";
+import AlumnoExtranjero from "./AlumnoExtranjero.js";
+import AlumnoIntercambio from "./AlumnoIntercambio.js";
+import IAsistencia from "./IAsistencia.js";
+
+
+const alumnoNacional = new AlumnoNacional(
+    1,
+    "Daniel",
+    "Inche",
+    20,
+    "999999999",
+    new Date("2005-05-15"),
+    "Lima",
+    "2025001",
+    "Sede Lima Centro",
+    "Desarrollo de Sistemas"
+);
+
+console.log(alumnoNacional.mostrarDatos());
+
+const alumnoExtranjero = new AlumnoExtranjero(
+    2,
+    "Ana",
+    "Mejia",
+    22,
+    "988888888",
+    new Date("2003-01-10"),
+    "Chile",
+    "AB123456",
+    "Español",
+    "Sede San Isidro"
+);
+
+console.log(alumnoExtranjero.mostrarDatos());
+
+
+const alumnoIntercambio = new AlumnoIntercambio(
+    3,
+    "Dana",
+    "Solano",
+    21,
+    "977777777",
+    new Date("2004-03-08"),
+    "Instituto Tecnológico de España",
+    6,
+    "Perú",
+    "Intercambio Académico"
+);
+
+console.log(alumnoIntercambio.mostrarDatos());
+
+
+alumnoIntercambio.setNombre("Diani")
+console.trace(alumnoIntercambio)
+
+alumnoIntercambio.registrarEntrada( new Date());
+alumnoIntercambio.registrarSalida( new Date(new Date().setHours(new Date().getHours() + 3)));
+
+alumnoNacional.registrarEntrada( new Date());
+alumnoNacional.registrarSalida(  new Date(new Date().setHours(new Date().getHours() + 4)));
+
+alumnoExtranjero.registrarEntrada( new Date());
+alumnoExtranjero.registrarSalida( new Date(new Date().setHours(new Date().getHours() + 5)));
+
+
+
+const profesor = new Profesor(
+    1,
+    "Bernabe",
+    "Inche",
+    "Programación",
+    "999888777",
+    "fbinche@idat.pe",
+    "Sede ATE"
+);
+
+profesor.mostrarDatos();
+profesor.registrarEntrada(new Date());
+profesor.registrarSalida(new Date(new Date().setHours(new Date().getHours() + 5)));
