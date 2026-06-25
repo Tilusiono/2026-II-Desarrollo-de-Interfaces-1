4a69737870b2422d05ac07489b56551aefa5c061
Jheff <jalbornoz@idat.pe>
2026-06-25 07:46:22 -0500

    Sincronizar main hacia bernabe-inche con prioridad a main

---
+++ pages/bernabe-inche/test.js
@@ -0,0 +1,21 @@
+let numero="dsad";
+numero="test";
+numero=2;
+console.log(numero);
+console.log(typeof numero);
+
+const nombre="Bernabé";
+console.log(nombre);
+console.log(typeof nombre);
+
+const apellido_paterno="Inche";
+console.log(apellido_paterno);
+console.log(typeof apellido_paterno);
+
+const apellido_materno="Ticlavilca";
+console.log(apellido_materno);
+console.log(typeof apellido_materno);
+
+/***test */
+console.log("Revisado");
+console.log("por Bernabé Inche Ticlavilca");
