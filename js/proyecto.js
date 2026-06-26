4c8708fbef81bff3c6d069cad6f2c88991473292
Bernabe Daniel Inche Ticlavilca <85372343+DAMN1SELF@users.noreply.github.com>
2026-06-26 01:41:35 -0500

    test

--- js/proyecto.js
+++ js/proyecto.js
@@ -3,14 +3,6 @@ import AlumnoExtranjero from "./AlumnoExtranjero.js";
 import AlumnoIntercambio from "./AlumnoIntercambio.js";
 import IAsistencia from "./IAsistencia.js";
 import Profesor from "./Profesor.js";
-import Materia from "./Materia.js";
-import Curso from "./Curso.js";
-import Aula from "./Aula.js";
-import Calificacion from "./Calificacion.js";
-import Asistencia from "./Asistencia.js";
-import Evaluacion from "./Evaluacion.js";
-import Libreta from "./Libreta.js";
-import Directorio from "./Directorio.js";
 
 
 const alumnoNacional = new AlumnoNacional(
@@ -89,31 +81,4 @@ const profesor = new Profesor(
 profesor.mostrarDatos();
 profesor.registrarEntrada(new Date());
 profesor.registrarSalida(new Date(new Date().setHours(new Date().getHours() + 5)));
-
-// Ejemplos de las nuevas clases con 4 parámetros
-console.log("\n=== NUEVAS CLASES ===\n");
-
-const materia = new Materia("Desarrollo de Interfaces", "DI2026", 5, "Medio");
-console.log(materia.mostrarDatos());
-
-const curso = new Curso("Interfaces Web", "IW-101", 30, "Lunes 8:00-10:00");
-console.log(curso.mostrarDatos());
-
-const aula = new Aula("101", 35, "A", 1);
-console.log(aula.mostrarDatos());
-
-const calificacion = new Calificacion(1, "DI2026", 9.5, new Date());
-console.log(calificacion.mostrarDatos());
-
-const asistencia = new Asistencia(1, new Date(), "Presente", "Llegó puntual");
-console.log(asistencia.mostrarDatos());
-
-const evaluacion = new Evaluacion("Examen Parcial", "Escrita", 30, new Date("2024-07-01"));
-console.log(evaluacion.mostrarDatos());
-
-const libreta = new Libreta(1, 2024, "1", "Activa");
-console.log(libreta.mostrarDatos());
-
-const directorio = new Directorio("Bernabe Inche", "999888777", "fbinche@idat.pe", "Profesor");
-console.log(directorio.mostrarDatos());
 
