9313643d61e271ca18180fc7a8cab8927e1f1180
Bernabe Daniel Inche Ticlavilca <85372343+DAMN1SELF@users.noreply.github.com>
2026-06-26 01:47:35 -0500

    Sync main -> roy-remigio (filtered pages)

--- js/Profesor.js
+++ js/Profesor.js
@@ -1,13 +1,11 @@
+import Persona from "./Persona.js";
+
 class Profesor extends Persona {
   #especialidad;
   #codigoDocente;
-  #salario; 
 
   sede;
   correo;
-  departamento; 
-  tipoContrato; 
-  gradosAcademicos;
 
   constructor(
     id,
@@ -20,10 +18,6 @@ class Profesor extends Persona {
     codigoDocente,
     sede,
     correo,
-    departamento,   
-    tipoContrato,  
-    salario,          
-    gradosAcademicos  
   ) {
     super(id, nombre, apellido, edad, telefono, fechaNacimiento);
 
@@ -31,23 +25,14 @@ class Profesor extends Persona {
     this.#codigoDocente = codigoDocente;
     this.sede = sede;
     this.correo = correo;
-    
-    // Asignación de los nuevos parámetros
-    this.departamento = departamento;
-    this.tipoContrato = tipoContrato;
-    this.#salario = salario;
-    this.gradosAcademicos = gradosAcademicos;
   }
 
   mostrarDatos() {
-
     return `
     Profesor: ${this.getNombre()}
     Especialidad: ${this.#especialidad}
-    Departamento: ${this.departamento}
     Sede: ${this.sede}
     Correo: ${this.correo}
-    Tipo de Contrato: ${this.tipoContrato}
     `;
   }
 
@@ -61,10 +46,6 @@ class Profesor extends Persona {
     console.log(
       `${this.getNombre()} profesor registró su salida (${fechaHora.toLocaleString()})`,
     );
-  }
-
-  getSalario() {
-    return this.#salario;
   }
 }
 
