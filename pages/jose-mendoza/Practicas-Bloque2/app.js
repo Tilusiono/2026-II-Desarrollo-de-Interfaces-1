0092532e47f9e1f6fb5932a8c60262a5c2bdc016
Jose Luis Mendoza Ventura <iv72817878@idat.pe>
2026-07-14 11:09:35 -0500

    Cambio jose-mendoza 07-14-2026_11-09

---
+++ pages/jose-mendoza/Practicas-Bloque2/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 7878;
+
+// Publicar los archivos HTML, CSS y JS de la carpeta public
+app.use(
+  express.static(
+    path.join(__dirname, "public"),
+    { index: false }
+  )
+);
+  
+app.use(
+  express.static(path.join(__dirname, "src"))
+);
+
+// Publicar Bootstrap instalado mediante NPM
+app.use(
+  "/bootstrap",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist"))
+);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutÃ¡ndose en http://localhost:${PORT}`);
+});
