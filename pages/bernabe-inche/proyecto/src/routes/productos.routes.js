a60736aa5d403c3745481c680f1ae9c4dfb79dfa
Bernabe Daniel Inche Ticlavilca <85372343+DAMN1SELF@users.noreply.github.com>
2026-08-08 17:56:04 -0500

    Rutas OK

--- pages/bernabe-inche/proyecto/src/routes/productos.routes.js
+++ pages/bernabe-inche/proyecto/src/routes/productos.routes.js
@@ -3,6 +3,7 @@ import { productosController as controller } from "../controllers/productos.cont
 //import { validarProductoCompleto } from "../middlewares/validacion.middleware.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ProductoRequestDto } from "../dtos/ProductoDto.js";
+import { validarId } from "../middlewares/id.middleware.js";
 
 const router = Router();
 
@@ -13,5 +14,9 @@ router.post(
     return controller.crear(productoRequestDto, response);
   }),
 );
+
+router.get("/",             asyncHandler((request, response) => controller.listar(response)));
+router.get("/:id",validarId,asyncHandler((request, response) => controller.obtener(Number(request.params.id), response),),);
+
 
 export default router;
--- pages/bernabe-inche/proyecto/src/routes/servicios.routes.js
+++ pages/bernabe-inche/proyecto/src/routes/servicios.routes.js
@@ -4,7 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ServicioRequestDto  } from "../dtos/ServicioDto.js";
 //import { validarServicioCompleto as validacion } from "../middlewares/validacion.middleware.js";
 import { serviciosController as controller } from "../controllers/servicios.controller.js";
-
+import { validarId } from "../middlewares/id.middleware.js";
 
 const router = Router();
 
@@ -15,5 +15,8 @@ router.post(
     return controller.crear(servicioRequestDto, response);
   }),
 );
+router.get("/",             asyncHandler((request, response) => controller.listar(response)));
+router.get("/:id",validarId,asyncHandler((request, response) => controller.obtener(Number(request.params.id), response),),);
+
 
 export default router;
