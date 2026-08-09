2a3424fcc32558a643f9ce4288fecb400789b935
Fabriscio Jordaho Carbajal Mauricio <at72989238@idat.pe>
2026-08-09 12:02:41 -0500

    patch router ok

--- pages/fabriscio-carbajal/Proyecto/src/routes/empleado.routes.js
+++ pages/fabriscio-carbajal/Proyecto/src/routes/empleado.routes.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 import { EmpleadoRequestDto } from "../dtos/EmpleadoDto.js";
-// import { validarEmpleadoCompleto } from "../middlewares/validacion.middleware.js";
 import { empleadoController as controller } from "../controllers/empleado.controller.js";
 import { validarId } from "../middlewares/id.middleware.js";
+
+// import { validarEmpleadoCompleto, validarEmpleadoParcial,} from "../middlewares/validacion.middleware.js";
+
 
 
 const router = Router();
@@ -24,12 +26,25 @@ router.get("/:id", validarId, asyncHandler((request, response) => controller.obt
 router.put(
   "/:id",
   validarId,
+  // validarEmpleadoCompleto
   asyncHandler((request, response) => {
     const id = Number(request.params.id);
     const empleadoRequestDto = new EmpleadoRequestDto(request.body);
     return controller.modificarControlador(id, empleadoRequestDto, response);
   }),
 );
+
+router.patch(
+  "/:id",
+  validarId,
+  // validarEmpleadoParcial,
+  asyncHandler((request, response) => {
+    const id = Number(request.params.id);
+    const empleadoRequestDto = new EmpleadoRequestDto(request.body);
+    return controller.modificarParcialControlador(id, empleadoRequestDto, response);
+  }),
+);
+
 
 
 export default router;
