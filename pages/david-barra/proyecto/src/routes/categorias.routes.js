76ae7a8aa9ea94f6b9b11aab3739018cbe652967
David Noe Barra Enriquez <at72054753@idat.pe>
2026-08-09 21:27:50 -0500

    rutas okk

--- pages/david-barra/proyecto/src/routes/categorias.routes.js
+++ pages/david-barra/proyecto/src/routes/categorias.routes.js
@@ -4,6 +4,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { CategoriaRequestDto} from "../dtos/CategoriaDto.js";
 //import { validarCategoriaCompleto as validacion } from "../middlewares/validacion.middleware.js";
 import { categoriasController as controller } from "../controllers/categorias.controller.js";
+import { validarId } from "../middlewares/id.middleware.js";
+
 
 const router = Router();
 
@@ -14,5 +16,9 @@ router.post(
     return controller.crear(categoriaRequestDto, response);
   }),
 );
+
+router.get("/", asyncHandler((request, response) => controller.listar(response)));
+router.get("/:id", validarId, asyncHandler((request, response) => controller.obtener(Number(request.params.id), response),),);
+
 
 export default router;
--- pages/david-barra/proyecto/src/routes/productos.routes.js
+++ pages/david-barra/proyecto/src/routes/productos.routes.js
@@ -3,15 +3,19 @@ import { productosController as controller } from "../controllers/productos.cont
 //import { validarProductoCompleto } from "../middlewares/validacion.middleware.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ProductoRequestDto } from "../dtos/ProductoDto.js";
+import { validarId } from "../middlewares/id.middleware.js";
 
 const router = Router();
 
 router.post(
-  "/",
+  "/",//validarProductoCompleto,
   asyncHandler((request, response) => {
     const productoRequestDto = new ProductoRequestDto(request.body);
     return controller.crear(productoRequestDto, response);
   }),
 );
+
+router.get("/", asyncHandler((request, response) => controller.listar(response)));
+router.get("/:id", validarId, asyncHandler((request, response) => controller.obtener(Number(request.params.id), response),),);
 
 export default router;
