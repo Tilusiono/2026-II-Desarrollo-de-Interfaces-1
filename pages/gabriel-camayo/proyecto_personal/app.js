4f641613cdddf62eba4148ba13cedcdd3517fc58
Gabriel Helaman Camayo Sanca <at77666132@idat.pe>
2026-08-10 23:11:13 -0500

    okok

--- pages/gabriel-camayo/proyecto_personal/app.js
+++ pages/gabriel-camayo/proyecto_personal/app.js
@@ -25,7 +25,7 @@ app.get("/api", (request, response) => {
   response.json({
     nombre: "Tienda de Hadware",
     almacenamiento: "sqlite",
-    recursos: ["productos,servicios"],
+    recursos: ["productos,perifericos"],
   });
 });
 
